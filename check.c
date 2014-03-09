4d6f4d6e2a13786fbe67d6b571284386f3b57cb4
Florian LE-BACHELIER-DE-LA-RIVIERE <fle-bach@e3r1p12.42.fr>
2014-03-09 14:19:59 +0100

    check

--- check.c
+++ check.c
@@ -6,7 +6,7 @@
 /*   By: fle-bach <fle-bach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/09 07:30:44 by fle-bach          #+#    #+#             */
-/*   Updated: 2014/03/09 08:54:37 by fle-bach         ###   ########.fr       */
+/*   Updated: 2014/03/09 14:11:37 by fle-bach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@ int		ft_check_three(int broad, int player, int check)
 
 int		ft_good_check_l_c(int x, int y, t_gboard *p4, int cl)
 {
-	if (cl == 0)
+	if (cl == 1)
+		return (y + 1);
+	if ((x + 1) == p4->nb_lines)
 	{
 		if (p4->board[x][y] == 0)
 			return (y + 1);
@@ -35,7 +37,16 @@ int		ft_good_check_l_c(int x, int y, t_gboard *p4, int cl)
 			return (y + 4);
 	}
 	else
-		return (y + 1);
+	{
+		if (p4->board[x][y] == 0 && p4->board[x + 1][y])
+			return (y + 1);
+		else if (p4->board[x][y + 1] == 0 && p4->board[x + 1][y + 1])
+			return (y + 2);
+		else if (p4->board[x][y + 2] == 0 && p4->board[x + 1][y + 2])
+			return (y + 3);
+		else if (p4->board[x][y + 3] == 0 && p4->board[x + 1][y + 3])
+			return (y + 4);
+	}
 	return (0);
 }
 
